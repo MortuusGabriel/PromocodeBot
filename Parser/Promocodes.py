a1aedfd16295f56d21e3f56d40b9e194492ae4aa
Vyacheslav Muravev <slavax997@gmail.com>
2021-03-22 23:26:20 +0300

    chromedriver path

--- Parser/Promocodes.py
+++ Parser/Promocodes.py
@@ -15,7 +15,9 @@ def interceptor(request):
 def get_content(link):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
-    chrome_options.headless = True
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    # chrome_options.headless = True
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.implicitly_wait(5)
     driver.request_interceptor = interceptor
